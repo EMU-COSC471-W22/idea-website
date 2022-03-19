e41f9c6b2a7f2d28c155e6297e356d46a50ecd98
JosephTech <49079887+JosephTech@users.noreply.github.com>
2022-03-18 23:07:29 -0400

    test

---
+++ server/api/uploadAPI.js
@@ -0,0 +1,39 @@
+// var express = require("express");
+// const app = express();
+
+// var router = express.Router();
+// var bodyParser = require('body-parser');
+// const Post = require('../models/post');
+
+// router.use(bodyParser.urlencoded({ extended: true }));
+
+
+// //new post callback
+// //puts a new post into the database
+// router.post("/newPost", function(req, res) {
+//     //get post data
+// 	var poster = req.body.username + "";
+//     var text = req.body.content + "";
+//     var attachments = req.body.attachments;
+//     var postData = { postedBy: poster };
+//     console.log("Entering a new post by " + poster);
+//     if (text) {
+//         postData.content = text;
+//         console.log(text);
+//     }
+//     if (attachments) {
+//         postData.attachments = attachments;
+//         console.log(attachments);
+//     }
+
+//     //add post to the database
+//     const post = new Post(postData);
+//     post.save().then((result) => {
+//         console.log(result);
+//         console.log("Success!");
+//     })
+//     .catch((err) => {
+//         console.log(err);
+//     });
+// });
+// module.exports = router;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,39 @@
-const express = require("express");
+const express = require('express')
+const multer = require('multer');
+const cors = require('cors')
 const app = express();
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./");
+    },
+    filename: function(req, file, cb) {
+        const ext = file.mimetype.split("/")[1];
+        cb(null, `uploads/${file.originalname}-${Date.now()}.${ext}`);
+    }
+});
+
+const upload = multer({
+    storage: storage
+});
+
+// CORS
+const cors = require('cors')
+app.use(cors({
+    origin: true,
+    methods: ["GET", "POST"],
+    credentials: true,
+}));
+
+const mysql      = require('mysql');
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'user',
+    password: 'password',
+    database: 'databasename',
+});
+
+connection.connect();
 
 app.listen(3001, function() {
     console.log("Server is running on port 3001...");
