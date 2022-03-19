3db071977cb2a034107fa13b3536f5875e94d333
Brandon Jones <brandonijones@outlook.com>
2022-03-19 00:47:32 -0400

    feat: Improve styling to the Navbar
    Installed the font awesome icons package and react-boostrap.
    Navbar is responsive to different webpage sizes.
    All links on the navbar redirect to the appropriate pages (aside from logging out)
    
    Addresses: Issue #2

---
+++ client/src/components/NavigationBar.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+// React Bootstrap Componenets
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Container from 'react-bootstrap/Container';
+
+
+function NavigationBar() {
+    return (
+        <div>
+            <Navbar collapseOnSelect className='navbar' bg='light' variant='light' expand='lg'>
+                <Navbar.Brand> <Link className='logo' to='/'>THE IDEA</Link> </Navbar.Brand>
+                <Navbar.Toggle className='navbar-toggle-padding' aria-controls='responsive-navbar-nav'/>
+                <Container className='nav-items' expand='lg'>
+                    <Navbar.Collapse className='justify-content-end' id='responsive-navbar-nav'>
+                        <Nav>
+                            <Link className='nav-item' to='/gallery'>Gallery</Link>
+                            <Link className='nav-item' to='/artists'>Artists</Link>
+                            <Link className='nav-item' to='/about'>About</Link>
+                            <Link className='nav-item' to='/contact'>Contact</Link>
+                        </Nav>
+                        <NavDropdown className='dropdown-color' id="collasible-nav-dropdown" title={<FontAwesomeIcon className='account-icon' icon={faUser} />} >
+                            <NavDropdown.Item href='/account'>Account</NavDropdown.Item>
+                            <NavDropdown.Item href='/settings'>Settings</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item>Log Out</NavDropdown.Item>
+                        </NavDropdown>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <Outlet />
+        </div>
+    );
+}
+
+export default NavigationBar;
