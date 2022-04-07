d32ac67f9eb47c2a050f5bd263ec249c10be6fa9
michaela gerweck <mgerwec1@emich.edu>
2022-04-07 10:19:09 -0400

    comment beginning

--- server/middlewares/AuthMiddleware.js
+++ server/middlewares/AuthMiddleware.js
@@ -7,7 +7,7 @@ const validateToken = (req, res, next) => {
 
     try {
         const validToken = verify(accessToken, "supersecrettoken");
-
+        req.user = validToken;
         if (validToken){
             return next();
         } 
